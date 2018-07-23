3c4e1f4eb2860cf894b48d174063c8439b7a7514
Jasmine Fitts <jasminefitts@Jasmines-MacBook-Pro.local>
2018-07-23 19:51:15 -0400

    updated home and survey files

---
+++ server.js
@@ -0,0 +1,29 @@
+//Dependencies 
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+
+//Sets up Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+
+require("./routes/apiRoutes")(app);
+require("./routes/hmtlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT:" + PORT);
+});
+
+app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+});
