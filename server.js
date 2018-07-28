d9aa0abbb6677a13d4598333a1af8c83bd68b19b
Jasmine Fitts <jasminefitts@Jasmines-MacBook-Pro.local>
2018-07-28 09:58:16 -0400

    updated server.js

--- server.js
+++ server.js
@@ -16,18 +16,6 @@ require("./app/routing/apiRoutes");
 require("./app/routing/htmlRoutes");
 
 
-// app.listen(PORT, function() {
-//     console.log("App listening on PORT:" + PORT);
-// });
-
-// app.get("/home", function(req, res) {
-//     res.sendFile(path.join(__dirname, "home.html"));
-// });
-
-// app.get("/survey", function(req, res) {
-//     res.sendFile(path.join(__dirname, "survey.html"));
-// });
-
 app.listen(PORT, function() {
     console.log("App listening on PORT" + PORT);
 });
